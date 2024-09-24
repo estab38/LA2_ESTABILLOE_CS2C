0fc50c4edd53bf5203e38eca10233afaa7f1b97f
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-09-24 16:36:04 +0800

    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,25 @@
+var Name = "[Ermalyn Calindas Estabillo]";
+var birthdate = "[October 19, 2004]";
+var birthPlace = "[Sapang,Sta Lucia,Ilocos Sur,Philippine 2710 ]";
+var addressed = "[Bagani Campo,Candon City,Ilocos Sur, Philippine 2709 ]";
+var Course = "[bachelor Of science in computer science ]";
+var Dream = "[programmer]";
+var Year = "2";
+
+console.log (Name.toUpperCase() + "was born"+ birthdate + "at" + birthPlace.toUpperCase() + "and currently living at "+ addressed.toUpperCase() + ".");
+console.log (Name.toLowerCase() + "is taking up"+  Course.toLowerCase() + "and dreams to be" + Dream + "after graduation");
+
+
+
+var Name = "[John Way Molina Gapate]";
+var birthdate = "[decemper 24, 2005 ]";
+var birthPlace = "[ Sta Cruz, Ilocos Sur, Phillipene 2710 ]";
+var addressed = "[Sabangan Bato, Galimuyod,Ilocos Sur,Phillipine 2709. ]";
+var Course = "[bachelor Of science in computer science ]";
+var Dream = "[Computer Designer]";
+var Year = "2";
+
+console.log (Name.toUpperCase() + "was born"+ birthdate+ "at" + birthPlace.toUpperCase() + "and currently living at "+ addressed.toUpperCase() + ".");
+console.log (Name.toLowerCase() + "is taking up + Course.toLowerCase() + "and dreams to be" + Dream + "after graduation");
+
+
