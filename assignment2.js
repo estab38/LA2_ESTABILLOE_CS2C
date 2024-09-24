87ef1114a7becd2d9b34131aee53a2f6bd93989b
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-09-24 16:37:21 +0800

    Create assignment2.js

---
+++ assignment2.js
@@ -0,0 +1,22 @@
+let a = 25;
+let b = 20;
+let c = 12
+let d = "15"
+const e = 15
+let sum = a + b +c + d + e;
+console.log(sum)
+console.log(d > e);   //false
+console.log(d < e);   //false
+console.log(d >= e); //true
+console.log(d <= e); //true
+console.log(d == e); //true
+console.log(d != e); //false
+console.log(d === e); //false
+let subtract= a - b - c - d;
+console.log(subtract);
+let multiplicationDivision = (a * c) / e;
+console.log(multiplicationDivision);
+let exponent = Math.pow(e, c);
+console.log(exponent);
+let E = c*3;
+console.log(e);
